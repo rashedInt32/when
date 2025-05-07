abc42f56db59a8e6572c3c5c8e8a8e27fd7a9ddf
rashed parvez <parvez08eee@gmail.com>
2025-05-07 12:47:03 +0600

    added when method

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,9 @@
-
+function when<T, U>(
+  condition: boolean | (() => boolean),
+  onTrue: T | (() => T),
+  onFalse: U | (() => U)
+): T | U {
+  const evaluate = <V>(value: V | (() => V)): V =>
+    typeof value === "function" ? (value as () => V)() : value;
+  return evaluate(condition) ? evaluate(onTrue) : evaluate(onFalse);
+}
